60bbc725e3f1a27fc330530d730cdbf791e3011f
anandhusb444 <anandhusb444@gmail.com>
2024-09-13 16:53:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace ADO.net_databse_CRUD_Console_Application
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string connectionStr = "data source=HP-HIKE-444\\SQLEXPRESS; data base=ado_DB; intergrated security=SSPI";
+
+                using(SqlConnection con = new SqlConnection(connectionStr))
+                {
+                    con.Open();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"there is an error : {ex.Message}");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ADO.net databse CRUD Console Application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ADO.net databse CRUD Console Application")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1dabace4-c2f7-4cf6-8b4b-a26c8518f48b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
