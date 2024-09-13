aacea91d7d95b5936e14e81ec19e6c67c63b5d0e
anandhusb444 <anandhusb444@gmail.com>
2024-09-13 19:30:01 +0530

    userById

--- Program.cs
+++ Program.cs
@@ -12,33 +12,82 @@ namespace ADO.net_databse_CRUD_Console_Application
 {
     internal class Program
     {
+        static string conStr = ConfigurationManager.ConnectionStrings["connectionStr"].ConnectionString;
 
         static void Main(string[] args)
         {
 
-            try
-            {
-                string connectionStr = "data source=HP-HIKE-444\\SQLEXPRESS; data base=ado_DB; intergrated security=SSPI";
+            Program pro = new Program();
+            pro.read();
 
-                using(SqlConnection con = new SqlConnection(connectionStr))
+            var userWith_1 = new byId();
+
+            userWith_1.SelectById();
+            
+
+
+            //try
+            //{
+
+            //    using (SqlConnection con = new SqlConnection(conStr))
+            //    {
+            //        con.Open();
+            //        Console.WriteLine("connection sucessfuly");
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine($"there is an error : {ex.Message}");
+            //}
+
+        }
+        public void read()
+        {
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+
+                string selectQuery = "select * from Students";
+
+                SqlCommand cmd = new SqlCommand(selectQuery, con);
+
+                SqlDataReader reder = cmd.ExecuteReader();
+
+                while (reder.Read())
+                {
+                    int user_id = reder.GetInt32(0);
+                    String user_name = reder.GetString(1);
+                    int user_age = reder.GetInt32(2);
+
+                    Console.WriteLine($"{user_id} , {user_name}, {user_age}");
+                }
+            }
+
+        }
+
+        public class byId
+        { 
+            public void SelectById()
+            {
+                using (SqlConnection con = new SqlConnection(conStr))
                 {
                     con.Open();
+                    
+                    string selectByIdQuery = "select stud_name from Students where stud_id = 1";
+
+                    SqlCommand conId = new SqlCommand(selectByIdQuery, con);
+
+                    SqlDataReader reder = conId.ExecuteReader();
+
+                    while (reder.Read())
+                    {
+                        Console.WriteLine(reder["stud_name"]);
+                    }
+
+
 
                 }
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine($"there is an error : {ex.Message}");
-            }
-
-
-
-
-
-
-
-
-
         }
     }
 }
